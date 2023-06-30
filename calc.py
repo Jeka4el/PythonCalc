b1dbc4147c1437178ffa7cdd51ca14d2451ae380
Jeka4el <ehodwork@gmail.com>
2023-06-30 14:43:33 +0300

    Fixed some bags

--- calc.py
+++ calc.py
@@ -1,37 +1,44 @@
 print("Calculator by Khodchenkov \n")
 
-num1 = float(input("Please enter the first number: "))
-num2 = float(input("Please enter the second number: "))
+try:
+    num1 = float(input("Please enter the first number: "))
+    num2 = float(input("Please enter the second number: "))
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+    exit()
+
+
 operator = input("Please select an operation: (\n 1. Addition, \n 2. Subtraction, \n 3. Multiplication, \n 4. Division \n Enter your choice (1-4): ")
 
 
 def addition(x,y):
     return x + y
-
 def subtraction(x,y):
     return x - y 
-
 def multiplication (x,y):
-    return x * y
-
+    if x == 0 or y == 0:
+        return 0
+    else:
+        return x * y
 def Division (x,y):
-    return x / y
+    if x == 0 or y == 0:
+        return 0
+    else:
+        return x / y
 
-if operator == "1":
-    result = addition (num1, num2)
-elif operator == "2":
-    result = subtraction (num1, num2)
-elif operator == "3":
-    if num1 == 0 or num2 == 0:
-        print("Error: Multiplication by zero is not allowed.")
-    else:
+
+if operator in ["1", "2", "3", "4"]:
+    if operator == "1":
+        result = addition (num1, num2)
+    elif operator == "2":
+        result = subtraction (num1, num2)
+    elif operator == "3":
         result = multiplication (num1, num2)
-elif operator == "4":
-    if num1 == 0 or num2 == 0:
-        print("Error: Division by zero is not allowed.")
-    else:
+    elif operator == "4": 
         result = Division (num1, num2)
+
 else:
-    print("Incorrect operator")
+    print("Invalid operator. Please enter a valid operator (1-4)")
+    exit()
 
 print ("The result of multiplication is: ", result )
